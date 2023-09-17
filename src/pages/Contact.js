4c08980885e1c9e626a53b03ac1de152798cb62d
atharvadesai1 <113234377+atharvadesai1@users.noreply.github.com>
2023-09-18 01:40:33 +0530

    working

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import * as React from 'react'
 import Layout from '../components/Layout'
+import BgImage from '../images/coof51.jpg'
+import '../styles/Contact.css'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import Stack from '@mui/material/Stack';
+
 
 function Contact() {
   return (
     <div>
       <Layout>
-        <h1>Contact</h1>
+        <div className="contact" style={{backgroundImage:`url(${BgImage})`}}>    
+          <div className="contact_form">
+            <div className="contactheading">Contact Us</div> 
+            <div className="insidebox">
+              <div className="left" style={{backgroundColor:'white',opacity:0.8,padding:'10px'}}>
+                <Box
+                component="form"
+                sx={{
+                  '& > :not(style)': {my:1,mx:2, width: '55ch'},
+                  color:'white'
+                }}
+                noValidate
+                autoComplete="off"
+              >
+                  <TextField id="outlined-basic" label="First Name" type='text' required variant="outlined" sx={{color:'white'}}/> <br />
+                  <TextField id="outlined-basic" label="Last Name" type='text' required variant="outlined" sx={{color:'white'}}/> <br />
+                  <TextField id="outlined-basic" label="Email ID" type='email' required variant="outlined" sx={{color:'white'}}/> <br />
+                  <TextField
+                      id="outlined-multiline-static"
+                      label="Comment"
+                      multiline
+                      rows={4}
+                      placeholder='Message/Suggestion'
+                  />
+                  <Stack direction="row" spacing={2}>
+                    <Button variant="contained" endIcon={<SendIcon />} sx={{mb:3}}>
+                      Send
+                    </Button>
+                  </Stack>
+                </Box>
+              </div>
+            </div>
+          </div>     
+          </div>
       </Layout>
     </div>
   )
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -2,11 +2,66 @@ import Layout from '../components/Layout'
 import MenuCard from './MenuCard'
 import '../styles/Menu.css'
 import dinnerFood from './dinner'
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+
 
 function Menu() {
+  // const [menuheading,setmenuheading] = React.useState('Special Dishes')
+    const [category, setcategory] = React.useState('');
+  
+    const handleChange = (event) => {
+      setcategory(event.target.value);
+    };
+
+    const StyledBadge = styled(Badge)(({ theme }) => ({
+      '& .MuiBadge-badge': {
+        right: -3,
+        top: 13,
+        border: `2px solid ${theme.palette.background.paper}`,
+        padding: '0 4px',
+      },
+    }));
+
   return (
     <div>
       <Layout>
+        <div className="mininavbar">
+            <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+              <InputLabel id="demo-select-small-label">Categories</InputLabel>
+              <Select
+                labelId="demo-select-small-label"
+                id="demo-select-small"
+                value={category}
+                label="Categories"
+                onChange={handleChange}
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={10}>Snack</MenuItem>
+                <MenuItem value={20}>Dessert</MenuItem>
+                <MenuItem value={30}>Chinese</MenuItem>
+              </Select>
+            </FormControl>
+
+            <div className="centremenu">Special Dishes</div>
+
+            <IconButton aria-label="cart">
+              <StyledBadge badgeContent={4} color="secondary">
+                <ShoppingCartIcon />
+              </StyledBadge>
+            </IconButton>
+        </div>
+
         <div className="cards">
           {dinnerFood.map((element)=>{
             return(
